25d6cffa3a8598f9d875dd25a6a0b21e362f2911
darkness <dap@open.by>
2011-08-28 20:42:35 +0300

    Optimization is done. Comments were written.

--- body.py
+++ body.py
@@ -1,52 +1,52 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+#Указывается язык и кодировка.
+
+#Основная функция. Производит выбор функции генерерирования. Если требуется, применяют функцию
+#уменьшения относительного стандартного отклонения (RSD) и заменяет точки на запятые.
 
 from simple import rando
 from average import average
 from rsd import relstdev
+#Импорт функций создания списка случаных чисел и уменьшения RSD.
 
 def randorator(t_mini, t_maxi, t_n, t_mean, t_rsd, punctuation):
+#Функция принимает текстовые значения нижней и верхней границ интервала, количества чисел, среднего значения,
+#максимально допустимого RSD и числовое значение, указывающее знак препинания в экспортируемых числах.
+
+    def punctu(txt):
+        return(float(txt.replace(",", ".")))
+#Функция принимает текстовое значение и возвращает число с точкой. Для обработки введённых данных.
+
     matrix = []
     text = ""
-    mini = float(t_mini.replace(",", "."))
-    maxi = float(t_maxi.replace(",", "."))
+    mini = punctu(t_mini)
+    maxi = punctu(t_maxi)
     n = int(t_n)
+    m = n - 1
+#Создаётся пустой список и текст. Обработываются границы интервала и количество чисел. Вычисляется удобное число m.
 
-#    mean = float(t_mean.replace(",", "."))
-#    rsd = float(t_rsd.replace(",", "."))
-
-#        mini = float((entry_mini.get()).replace(",", "."))
-#    maxi = float((entry_maxi.get()).replace(",", "."))
-#Очистка текстового поля. Импорт минимума и максимума,
-#замена запятых на точки, если необходимо.
-#
-#    if vcheck_punctuation.get():
-#        for i in xrange((int(entry_n.get()) - 1)):
-#            text_out.insert(END, str(uniform(mini, maxi))+"\n")
-#        text_out.insert(END, str(uniform(mini, maxi)))
-#Если не требуется вывод чисел с запятыми,
-#текстовое поле заполняется случайными числами,
-#каждое с новой строки. Последнее значение вставляется без перевода строки.
-#
-#    else:
-#        for i in xrange((int(entry_n.get()) - 1)):
-#            text_out.insert(END, str(uniform(mini, maxi)).replace(".", ",")+"\n")
-#        text_out.insert(END, str(uniform(mini, maxi)).replace(".", ","))
-#Иначе в аналогичных операциях точки заменяются на запятые.
-    
     if t_mean == "":
         matrix = rando(mini, maxi, n)
     else:
-        matrix = average(mini, maxi, n, float(t_mean.replace(",", ".")))
+        mean = punctu(t_mean)
+        matrix = average(mini, maxi, n, mean, m)
+#Выбор и применение функции создание списка чисел в зависимости от того, задано ли среднее значение.
         
     if t_rsd != "":
-        matrix = relstdev(matrix, n, float(t_rsd.replace(",", ".")))
+        if t_mean == "":
+            mean = sum(matrix) / n
+        matrix = relstdev(matrix, n, mean, punctu(t_rsd))
+#Если требуется, уменьшается RSD.
     
-    m = n - 1
-    for i in xrange(0, m):
+    for i in xrange(m):
         text += str(matrix[i]) + "\n"
     text +=  str(matrix[m])
+#Список преобразуется в строку. Каждое число с красной строки.
     
     if not punctuation:
         text = text.replace(".", ",")
+#Если требуется, то точки заменяются запятыми.
+
     return(text)
+#Возврат теста.
