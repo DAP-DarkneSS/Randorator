d3a643d04a5ef59c36c8bde522ff116b36fcadc2
Dmitriy <dap@open.by>
2012-04-27 00:53:16 +0300

    Base wx-GUI was started. There are two buttons now, and one works!

---
+++ wx-randorator.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#Указывается язык и кодировка.
+
+import wx
+
+def button_fmake(event):
+# Функция для кнопки генерации. Записывается с аргументом события.
+    event.Skip()
+# Ничего не делать... пока .__.
+
+def button_fexit(event):
+# Функция для кнопки выхода. Записывается с аргументом события.
+    gui.Close(True)
+    root.ExitMainLoop()
+# Сразу два способа выйти ^,_,^
+
+root = wx.App()
+
+gui = wx.Frame(parent = None, style = wx.MINIMIZE_BOX | wx.SYSTEM_MENU | wx.CAPTION | wx.CLOSE_BOX, title = 'Randorator')
+# Окно можно сворачивать, с системным меню, с заголовком и кнопкой закрытия.
+# Задаётся заголовок.
+ 
+button_make = wx.Button(gui, label=u"Генерировать!", pos=(0, 0), size=(110, 25))
+button_exit = wx.Button(gui, label=u"Выход", pos=(111, 0), size=(110, 25))
+# Кнопки.
+
+gui.Bind(wx.EVT_BUTTON, button_fmake, button_make)
+gui.Bind(wx.EVT_BUTTON, button_fexit, button_exit)
+# Присваиваем кнопкам соответствующие функцию.
+
+gui.Show()
+# Показать форму.
+
+root.MainLoop()
+# Окончание формы.
