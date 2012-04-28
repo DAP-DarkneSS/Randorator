07e25fad251e75e4e5b6c982abc17d0a9b29e52a
Dmitriy <dap@open.by>
2012-04-28 09:50:32 +0300

    Buttons were packed with wx.BoxSizer.

--- wx-randorator.py
+++ wx-randorator.py
@@ -11,27 +11,34 @@ def button_fmake(event):
 
 def button_fexit(event):
 # Функция для кнопки выхода. Записывается с аргументом события.
-    gui.Close(True)
-    root.ExitMainLoop()
-    wx.Exit()
-# Сразу три способа выйти ^,_,^
+    frame_root.Close()
+#     app_root.ExitMainLoop()
+#     wx.Exit()
+# Сразу три способа выйти ^,_,^ Два последних вешаются под Windows.
 
-root = wx.App()
+app_root = wx.App()
 
-gui = wx.Frame(parent = None, style = wx.MINIMIZE_BOX | wx.SYSTEM_MENU | wx.CAPTION | wx.CLOSE_BOX, title = 'Randorator')
+frame_root = wx.Frame(parent = None, style = wx.MINIMIZE_BOX | wx.SYSTEM_MENU | wx.CAPTION | wx.CLOSE_BOX, title = 'Randorator')
 # Окно можно сворачивать, с системным меню, с заголовком и кнопкой закрытия.
 # Задаётся заголовок.
+
+sizer_root = wx.BoxSizer(wx.HORIZONTAL)
  
-button_make = wx.Button(gui, label=u"Генерировать!", pos=(0, 0), size=(110, 25))
-button_exit = wx.Button(gui, label=u"Выход", pos=(111, 0), size=(110, 25))
+button_make = wx.Button(frame_root, wx.NewId(), label=u"Генерировать!", size=(110, 25))
+button_exit = wx.Button(frame_root, wx.NewId(), label=u"Выход", size=(110, 25))
 # Кнопки.
 
-gui.Bind(wx.EVT_BUTTON, button_fmake, button_make)
-gui.Bind(wx.EVT_BUTTON, button_fexit, button_exit)
+sizer_root.Add(button_make)
+sizer_root.Add(button_exit)
+
+frame_root.Bind(wx.EVT_BUTTON, button_fmake, button_make)
+frame_root.Bind(wx.EVT_BUTTON, button_fexit, button_exit)
 # Присваиваем кнопкам соответствующие функцию.
 
-gui.Show()
+frame_root.SetSizer(sizer_root, deleteOld=True)
+
+frame_root.Show()
 # Показать форму.
 
-root.MainLoop()
+app_root.MainLoop()
 # Окончание формы.
