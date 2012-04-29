f91a3cb2d03e94f78a602bc1221f4b6193045970
Dmitriy <dap@open.by>
2012-04-29 23:40:14 +0300

    Clipboard doesn't work.

--- wx-randorator.py
+++ wx-randorator.py
@@ -57,6 +57,14 @@ def button_fmake(event):
 # Передача внешней функции большинства параметров и получение теста.
     text_out.SetValue(text)
 # Запись текста в поле.
+    if check_copy.IsChecked():
+        pass
+        #cb = wx.Clipboard()
+        #if cb.Open():
+            #tdo = wx.TextDataObject("123")
+            #cb.SetData(tdo)
+            #cb.Close()
+# Начало работы над буфером обмена. Epic fail! =(
 
 #==========================|Exit_Button_Function|===========================#
 
