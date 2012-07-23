af234ccbd85efb5af4cd8868b8f1e6acdaa91d3e
Dmitriy <dap@open.by>
2012-07-24 00:39:31 +0300

    Issue #14 (slow generation via random.org) was partially fixed.

--- randomwrapper.py
+++ randomwrapper.py
@@ -29,36 +29,40 @@
 
 # Here it is a simple wrapper to use some random functions transparently.
 
-from urllib2 import urlopen
+from urllib2 import urlopen, URLError
 from thirdparty.randomdotorg import RandomDotOrg
-# A site opening function and a true random generation module are imported.
+import random
+# A site opening function and true and pseudorandom random generation modules are imported.
 
 #=============================|Module_Wrapper|==============================#
 
-if (urlopen("http://www.random.org/").getcode() < 400) and (RandomDotOrg().get_quota() > 15):
-    wrapper = RandomDotOrg()
+def checkthemall():
+    try:
+        if (urlopen("http://www.random.org/", timeout = 0.02).getcode() < 400) and (RandomDotOrg().get_quota() > 15):
+            wrapper = RandomDotOrg()
 # If response of random.org is OK and if the site quota allows using it
 # the wrapper will became a synonym of the true random generation module.
 # randomdotorg is licenced under GPLv3 and/or any later. The creator is
 # Clovis Fabricio. See more at http://code.google.com/p/randomdotorg/
 
-else:
-    import random
-    wrapper = random
-# Else a pseudorandom generation module will be imported
-# and the wrapper will became it's synonym.
+        else:
+            wrapper = random
+    except URLError:
+        wrapper = random
+    return(wrapper)
+# Else the wrapper will became synonym of pseudorandom random generation module.
 
 #===========================|Functions_Wrappers|============================#
 
 def random():
-    return(wrapper.random())
+    return(checkthemall().random())
 
 def shuffle(matrix):
-    return(wrapper.shuffle(matrix))
-    
+    return(checkthemall().shuffle(matrix))
+
 def triangular(mini, maxi, mean):
-    return(wrapper.triangular(mini, maxi, mean))
-    
+    return(checkthemall().triangular(mini, maxi, mean))
+
 def uniform(mini, maxi):
-    return(wrapper.uniform(mini, maxi))
+    return(checkthemall().uniform(mini, maxi))
 # Some functions are wrapped.
