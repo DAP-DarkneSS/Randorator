9fc99036aff8d3fc51dfff15a948d61c3b1ba503
Dmitriy <dap@open.by>
2013-03-15 00:44:37 +0300

    Tools to build windows portable tkinter GUI.

---
+++ randorator.spec
@@ -0,0 +1,19 @@
+# -*- mode: python -*-
+a = Analysis([os.path.join(HOMEPATH,'support\\_mountzlib.py'), os.path.join(HOMEPATH,'support\\useUnicode.py'), 'randorator.py'],
+             pathex=['D:\\D\\DAP-DarkneSS-Randorator-2d70e46'])
+pyz = PYZ(a.pure)
+exe = EXE(pyz,
+          a.scripts,
+          exclude_binaries=1,
+          name=os.path.join('build\\pyi.win32\\randorator', 'randorator.exe'),
+          debug=False,
+          strip=False,
+          upx=True,
+          console=False , icon='randorator.ico')
+coll = COLLECT( exe,
+               a.binaries,
+               a.zipfiles,
+               a.datas,
+               strip=False,
+               upx=True,
+               name=os.path.join('dist', 'randorator'))
---
+++ tk-pyinstaller.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+WINE_EXEC=wine
+
+PYTHON_DIR=~/.wine/drive_c/Python27
+PYTHON_EXEC=$PYTHON_DIR/python.exe
+PYINSTALLER=$PYTHON_DIR/Lib/site-packages/pyinstaller-2.0/pyinstaller.py
+
+PROJECT_DIR=~/Documents/Python/Randorator
+SPEC=randorator.spec
+
+cd $PROJECT_DIR
+$WINE_EXEC $PYTHON_EXEC $PYINSTALLER $SPEC
+
+VER=`git describe | sed "s/^v//g" | tr "-" "."`
+DIST_DIR=randorator-$VER-pyinstaller-tkinter
+
+mv dist $DIST_DIR
+cp COPYING* *.md randorator.gif $DIST_DIR/randorator
+
+rm logdict*.log
+rm -rf build
+cd $DIST_DIR/randorator
+rm -rf _ssl.pyd unicodedata.pyd _hashlib.pyd pyexpat.pyd _*/tcl/msgs _*/tcl/tzdata _*/tcl/clock.tcl _*/tk/images
+
+cd ..
+zip -r -9 ../$DIST_DIR.zip randorator
+
+cd randorator
+$WINE_EXEC randorator.exe
