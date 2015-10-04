DAP-DarkneSS/Randorator

commit 8660120d0391a6eb04aed03031e3d43271638adc

Implemented window always on top behavior. Closes #58.

wx_randorator.py
@@ -173,6 +173,9 @@ def UserInterface(Settingz):
 
     frame_root = wx.Frame(parent = None, style = wx.MINIMIZE_BOX | wx.SYSTEM_MENU | wx.CAPTION | wx.CLOSE_BOX)
 # Создаётся окно: его можно сворачивать, есть системное меню и кнопка закрытия.
+    if Settingz["log_ontop"]:
+        frame_root.SetWindowStyleFlag(wx.STAY_ON_TOP)
+# Window always on top behavior if configured.
     frame_root.SetTitle(locale(u"ui_title", Settingz["str_langu"]))
 # Задаётся заголовок окна.
     window_icon = check_icon("ico")

randorator.ini
@@ -8,6 +8,9 @@ str_usint:
 ; * en → ru
 str_langu: 
 
+; Window always on top behavior. True by default.
+log_ontop: 
+
 [Variables]
 
 ; 0 by default.

randorator.py
@@ -62,6 +62,7 @@ Confirator.read(check_icon("ini"))
 Settingz = {
     "str_usint": u"",
     "str_langu": u"en",
+    "log_ontop": True,
     "str_minim": u"",
     "str_maxim": u"",
     "str_quant": u"",
@@ -143,6 +144,8 @@ if DecimalMarkConf == u",":
 # If decimal mark isn't configured system settings will be parsed.
 
 #==================================|Setup|==================================#
+
+Settingz["log_ontop"] = parseIt(Confirator, u"Interface", "log_ontop", Settingz["log_ontop"], u"bool")
 
 for i in ["log_clipb", "log_verbo", "log_algor", "log_min_v", "log_max_v", "log_rsd_a", "log_rsd_w"]:
     Settingz[i] = parseIt(Confirator, u"Variables", i, Settingz[i], u"bool")

tk_randorator.py
@@ -182,6 +182,9 @@ def UserInterface(Settingz):
 #Нельзя изменять размер окна.
     root.call('wm', 'iconphoto', root._w, PhotoImage(file = check_icon("gif")))
 # Window icon is loaded.
+    if Settingz["log_ontop"]:
+        root.attributes("-topmost", True)
+# Window always on top behavior if configured.
 
     label_title = Label(root, text = locale(u"ui_about", Settingz["str_langu"]))
     label_title.pack()

commit a6a78ac6b5e4c059a5127d9191e9e28842033120

Fixed Windows and/or WxPython issue introduced by 8660120d0391a6eb04aed03031e3d43271638adc.

wx_randorator.py
@@ -173,10 +173,11 @@ def UserInterface(Settingz):
     app_root = wx.App(0)
 # Инициация тулкита. Zero to disable output window creating under windows.
 
-    frame_root = wx.Frame(parent = None, style = wx.MINIMIZE_BOX | wx.SYSTEM_MENU | wx.CAPTION | wx.CLOSE_BOX)
+    FrameStyle = wx.MINIMIZE_BOX | wx.SYSTEM_MENU | wx.CAPTION | wx.CLOSE_BOX
+    frame_root = wx.Frame(parent = None, style = FrameStyle)
 # Создаётся окно: его можно сворачивать, есть системное меню и кнопка закрытия.
     if Settingz["log_ontop"]:
-        frame_root.SetWindowStyleFlag(wx.STAY_ON_TOP)
+        frame_root.SetWindowStyleFlag(FrameStyle | wx.STAY_ON_TOP)
 # Window always on top behavior if configured.
     frame_root.SetTitle(locale(u"ui_title", Settingz["str_langu"]))
 # Задаётся заголовок окна.