d2b625063caea68f2e183f9f3a4f2cee8918dda4
Dmitriy A. Perlow <dap.darkness@gmail.com>
2016-05-26 21:57:10 +0300

    PyInstaller 3.0 support. Let close #60 so.

--- tools/pyinstaller/qt-pyinstaller.sh
+++ tools/pyinstaller/qt-pyinstaller.sh
@@ -4,15 +4,20 @@ WINE_EXEC=wine
 
 PYTHON_DIR=~/.wine/drive_c/Python27
 PYTHON_EXEC=$PYTHON_DIR/python.exe
-PYINSTALLER=$PYTHON_DIR/Lib/site-packages/pyinstaller-2.0/pyinstaller.py
+PYINSTALLER=$PYTHON_DIR/Lib/site-packages/*nstaller-3.0/pyinstaller.py
 
 PROJECT_DIR=~/Documents/Python/Randorator
-SPEC=$PROJECT_DIR/tools/pyinstaller/qt-randorator.spec
+TITLE=qt-randorator
+SPECNAME=$TITLE.spec
+SPEC=$PROJECT_DIR/tools/pyinstaller/$SPECNAME
 
 cd $PROJECT_DIR
 cat tools/stuff/0.patch | patch -p1
-$WINE_EXEC $PYTHON_EXEC $PYINSTALLER $SPEC
-cat tools/stuff/1.patch | patch -p0
+cp $SPEC $PROJECT_DIR
+$WINE_EXEC $PYTHON_EXEC $PYINSTALLER $SPECNAME
+rm $SPECNAME
+git checkout HEAD randorator.py
+rm randorator.py.orig
 
 VER=`git describe | sed "s/^v//g" | tr "-" "."`
 DIST=randorator-$VER-pyinstaller-pyqt
@@ -20,18 +25,9 @@ DIST_DIR=$PROJECT_DIR/build/$DIST/randorator
 
 rm -rf $DIST_DIR
 mkdir -p $PROJECT_DIR/build/$DIST
-mv dist/qt-randorator $DIST_DIR
+mv dist/$TITLE $DIST_DIR
 cp COPYING* *.md randorator.gif randorator.ini $DIST_DIR
 
-rm -rf build/qt-randorator
+rm -rf build/$TITLE
 cd $DIST_DIR
-rm -rf _ctypes.pyd bz2.pyd pywintypes27.dll select.pyd win32api.pyd win32pipe.pyd
-rm -rf _ssl.pyd unicodedata.pyd _hashlib.pyd pyexpat.pyd QtOpenGL4.dll QtSvg4.dll QtXml4.dll qt4_plugins/accessible qt4_plugins/accessible qt4_plugins/graphicssystems qt4_plugins/iconengines qt4_plugins/imageformats/qico4.dll qt4_plugins/imageformats/qjpeg4.dll qt4_plugins/imageformats/qmng4.dll qt4_plugins/imageformats/qsvg4.dll qt4_plugins/imageformats/qtga4.dll qt4_plugins/imageformats/qtiff4.dll
-
-# cd ..
-# rm -rf ../../dist/$DIST.zip
-# mkdir -p ../../dist
-# zip -r9 ../../dist/$DIST.zip randorator
-
-# cd randorator
-$WINE_EXEC qt-randorator.exe
+$WINE_EXEC $TITLE.exe
--- tools/pyinstaller/wx-pyinstaller.sh
+++ tools/pyinstaller/wx-pyinstaller.sh
@@ -4,15 +4,20 @@ WINE_EXEC=wine
 
 PYTHON_DIR=~/.wine/drive_c/Python27
 PYTHON_EXEC=$PYTHON_DIR/python.exe
-PYINSTALLER=$PYTHON_DIR/Lib/site-packages/PyInstaller-2.1/pyinstaller.py
+PYINSTALLER=$PYTHON_DIR/Lib/site-packages/*nstaller-3.0/pyinstaller.py
 
 PROJECT_DIR=~/Documents/Python/Randorator
-SPEC=$PROJECT_DIR/tools/pyinstaller/wx-randorator.spec
+TITLE=wx-randorator
+SPECNAME=$TITLE.spec
+SPEC=$PROJECT_DIR/tools/pyinstaller/$SPECNAME
 
 cd $PROJECT_DIR
 cat tools/stuff/0.patch | patch -p1
-$WINE_EXEC $PYTHON_EXEC $PYINSTALLER $SPEC
-cat tools/stuff/1.patch | patch -p0
+cp $SPEC $PROJECT_DIR
+$WINE_EXEC $PYTHON_EXEC $PYINSTALLER $SPECNAME
+rm $SPECNAME
+git checkout HEAD randorator.py
+rm randorator.py.orig
 
 VER=`git describe | sed "s/^v//g" | tr "-" "."`
 DIST=randorator-$VER-pyinstaller-wxwidgets
@@ -20,18 +25,20 @@ DIST_DIR=$PROJECT_DIR/build/$DIST/randorator
 
 rm -rf $DIST_DIR
 mkdir -p $PROJECT_DIR/build/$DIST
-mv dist/wx-randorator $DIST_DIR
+mv dist/$TITLE $DIST_DIR
 cp COPYING* *.md randorator.ico randorator.ini $DIST_DIR
 
-rm -rf build/wx-randorator
+rm -rf build/$TITLE
 cd $DIST_DIR
-rm -rf _ctypes.pyd bz2.pyd pywintypes27.dll select.pyd win32api.pyd win32pipe.pyd
-rm gdiplus.dll _ssl.pyd unicodedata.pyd _hashlib.pyd pyexpat.pyd
+rm -rf future *32.dll msvc*90.dll pywintypes27.dll \
+       _hashlib.pyd _ssl.pyd bz2.pyd pyexpat.pyd select.pyd unicodedata.pyd win32*.pyd
+rm winmm.dll winspool.drv
 
 cd ..
 rm -rf ../../dist/$DIST.zip
 mkdir -p ../../dist
-zip -r9 ../../dist/$DIST.zip randorator
+zip -r9Xv ../../dist/$DIST.zip randorator
+7z a -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on ../../dist/$DIST.7z randorator
 
 cd randorator
-$WINE_EXEC wx-randorator.exe
+$WINE_EXEC $TITLE.exe
